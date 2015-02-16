4c5c9ca5f3fe8680d4809c829a8842f94db949aa
caseydedore <caseydedore@gmail.com>
2015-02-15 17:51:07 -0800

    Tests for User and Account
    More work defining these tests.

--- ATM/UnitTests.cs
+++ ATM/UnitTests.cs
@@ -10,9 +10,7 @@ namespace ATM
     [TestFixture]
     class UT_UserAccount
     {
-        public int testAcctNum01,
-                   testAcctNum02,
-                   testAcctNum03;
+        public int[] testAcctNums;
         User user;
 
 
@@ -22,39 +20,36 @@ namespace ATM
             user = new User();
 
             //setup accounts for the test user
-            testAcctNum01 = 10101010;
-            testAcctNum02 = 11111111;
-            testAcctNum03 = 11000001;
-            user.CreateAccount(testAcctNum01);
-            user.CreateAccount(testAcctNum02);
-            user.CreateAccount(testAcctNum03);
+            testAcctNums = new int[]{ 1234, 1212, 1342, 1902 };
+            //these accounts are created the hard way so that the tests are independent of the success of CreateUserAccount()
+            //NOTE: do NOT initialize the last account for the acct number here. It will be done through a test
+            for (int i = 0; i < testAcctNums.Count() - 1; i++)
+            {
+                user.Accnts[i] = new Account(testAcctNums[i]);
+            }
         }
 
         [Test]
         public void TestUserAccountCreation()
         {
-            //insert the number of CreateAccount statements called during Setup to see if they all have been added
-            Assert.AreEqual(3, user.Accnts.Count);
+            //create an account using the account creation method in User. See if it was added
+            user.CreateAccount(testAcctNums[testAcctNums.Count() - 1]);
+            Assert.AreEqual(4, user.Accnts.Count);
         }
 
         [Test]
         public void TestUserAccountBalances()
         {
-            //is the correct balance coming back?
-
-            //alter the balance of the second user account
-            Account accnt = user.Accnts[1];
-            if (accnt != null)
+            //is the correct balance coming back? Do a single test first
+            user.Accnts[0].Balance = 102.12m;
+            Assert.AreEqual(102.12m, user.CheckBalance(user.Accnts[0].AccountNum));
+  
+            Random r = new Random();
+            //check all accounts
+            for (int i = 0; i < user.Accnts.Count(); i++)
             {
-                accnt.Balance = 102.12m;
-                Assert.AreEqual(102.12m, user.CheckBalance(user.Accnts[1].AccountNum));
-            }
-            else Assert.IsTrue(false);
-
-            //does the user method for checking balance match the balance property within the account itself?
-            foreach (Account act in user.Accnts)
-            {
-                Assert.AreEqual(act.Balance, user.CheckBalance(act.AccountNum));
+                user.Accnts[i].Balance = (decimal)r.Next(100000);
+                Assert.AreEqual(user.Accnts[i].Balance, user.CheckBalance(user.Accnts[i].AccountNum));
             }
         }
 
@@ -91,7 +86,7 @@ namespace ATM
             user.Deposit(user.Accnts[1].AccountNum, 100m);
             Assert.AreEqual(100m, user.Accnts[1].Balance);
 
-            //do a second test depositing money to acct where money exists already
+            //second test depositing money to acct where money exists already
             user.Accnts[1].Balance = 900m;
             user.Deposit(user.Accnts[1].AccountNum, 100m);
             Assert.AreEqual(1000m, user.Accnts[1].Balance);
@@ -100,12 +95,64 @@ namespace ATM
             user.Accnts[1].Balance = 900m;
             Assert.False(user.Deposit(user.Accnts[1].AccountNum, -91m));
             Assert.AreEqual(900m, user.Accnts[1].Balance);
+
+            //depositing 0 - should not happen
+            user.Accnts[1].Balance = 100m;
+            Assert.False(user.Deposit(user.Accnts[1].AccountNum, 0));
+            Assert.AreEqual(100m, user.Accnts[1].Balance);
         }
 
+        /// <summary>
+        /// This test and Transfer() itself relies on Deposit() and Withdraw()
+        /// </summary>
         [Test]
         public void TestTransfer()
         {
             //test to make sure the amount specified is removed from the sender acct and added to the receiver acct
-        }  
+
+            //transfer funds from one account to another
+            SetAllAccountBalances(ref user, 1000m);
+            user.Transfer(user.Accnts[0].AccountNum, user.Accnts[1].AccountNum, 100m);
+            Assert.AreEqual(900m, user.Accnts[0].Balance);
+            Assert.AreEqual(1100m, user.Accnts[1].Balance);
+
+            //transfer a small amount from one to another
+            SetAllAccountBalances(ref user, 1m);
+            user.Transfer(user.Accnts[0].AccountNum, user.Accnts[1].AccountNum, 0.03m);
+            Assert.AreEqual(0.97m, user.Accnts[0].Balance);
+            Assert.AreEqual(0.03m, user.Accnts[1].Balance);
+
+            //transfer from where there are insufficient funds
+            SetAllAccountBalances(ref user, 10m);
+            Assert.False(user.Transfer(user.Accnts[0].AccountNum, user.Accnts[1].AccountNum, 100m));
+            Assert.AreEqual(10m, user.Accnts[0].Balance);
+            Assert.AreEqual(10m, user.Accnts[1].Balance);
+
+            //transfer 0 - should NOT happen at all
+            SetAllAccountBalances(ref user, 240m);
+            Assert.False(user.Transfer(user.Accnts[0].AccountNum, user.Accnts[1].AccountNum, 0m));
+            Assert.AreEqual(240m, user.Accnts[0].Balance);
+            Assert.AreEqual(240m, user.Accnts[1].Balance);
+
+            //transfer negative funds
+            SetAllAccountBalances(ref user, 100m);
+            Assert.False(user.Transfer(user.Accnts[0].AccountNum, user.Accnts[1].AccountNum, -20m));
+            Assert.AreEqual(100m, user.Accnts[0].Balance);
+            Assert.AreEqual(100m, user.Accnts[1].Balance);
+
+            //transfer from and to the same account - should not happen
+            SetAllAccountBalances(ref user, 100m);
+            Assert.False(user.Transfer(user.Accnts[0].AccountNum, user.Accnts[0].AccountNum, 15.20m));
+            Assert.AreEqual(100m, user.Accnts[0].Balance);
+
+        }
+
+        private void SetAllAccountBalances(ref User u, decimal d)
+        {
+            foreach (Account acct in u.Accnts)
+            {
+                acct.Balance = 1000m;
+            }
+        }
     }
 }
